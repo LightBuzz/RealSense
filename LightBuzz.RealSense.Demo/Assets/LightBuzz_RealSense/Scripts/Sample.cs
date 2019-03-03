fdfeb89b8a32fb9890bf71a775e2c51492779cb9
Vangos <vangos@lightbuzz.com>
2019-03-03 18:27:18 +0200

    Working version without recording.

--- LightBuzz.RealSense.Demo/Assets/LightBuzz_RealSense/Scripts/Sample.cs
+++ LightBuzz.RealSense.Demo/Assets/LightBuzz_RealSense/Scripts/Sample.cs
@@ -13,24 +13,13 @@ public class Sample : MonoBehaviour
     [SerializeField]
     private Text log;
 
-    [SerializeField]
-    [Range(1, 20)]
-    private int radius = 4;
-
     private RealSenseDevice device;
+    private AlignedFrameData data;
 
-    private DateTime dateColor = DateTime.MinValue;
-    private DateTime dateDepth = DateTime.MinValue;
+    private DateTime fpsDate = DateTime.MinValue;
+    private int fpsCount;
 
-    private int countColor;
-    private int countDepth;
-
-    private byte[] colorData;
-    private ushort[] depthData;
     private Texture2D texture;
-
-    private float depth;
-    private float depthMedian;
 
     private int x = 640 / 2;
     private int y = 480 / 2;
@@ -39,36 +28,28 @@ public class Sample : MonoBehaviour
     {
         device = new RealSenseDevice();
         device.OnFrameDataArrived += Device_OnFrameDataArrived;
-        device.OnRecordingCompleted += Device_OnRecordingCompleted;
         device.Open();
 
-        colorData = new byte[640 * 480 * 3];
-        depthData = new ushort[640 * 480];
-        texture = new Texture2D(640, 480, TextureFormat.RGB24, false);
+        texture = new Texture2D(device.DeviceConfiguration.ColorProfile.Width, device.DeviceConfiguration.ColorProfile.Height, TextureFormat.RGB24, false);
         image.texture = texture;
     }
 
     private void OnDestroy()
     {
-        if (device != null)
-        {
-            device.OnFrameDataArrived -= Device_OnFrameDataArrived;
-            device.OnRecordingCompleted -= Device_OnRecordingCompleted;
-            device.Close();
-        }
+        device.OnFrameDataArrived -= Device_OnFrameDataArrived;
+        device.Close();
     }
 
     private void Update()
     {
-        if (device.FrameDataArrived)
+        if (device.IsOpen)
         {
-            texture.LoadRawTextureData(colorData);
+            texture.LoadRawTextureData(data.ColorData);
             texture.Apply(false);
 
-            log.text = 
-                Math.Round(depth, 1).ToString("N1") + 
-                Environment.NewLine +
-                Math.Round(depthMedian, 1).ToString("N1");
+            //float depth = data.DepthData[y * 640 + x] / 1000f;
+
+            //log.text = Math.Round(depth, 2).ToString("N2");
         }
 
         Vector3 mouse = Input.mousePosition;
@@ -80,78 +61,28 @@ public class Sample : MonoBehaviour
         }
     }
 
-    private void Device_OnFrameDataArrived(AlignedFrameData data)
+    private void Device_OnFrameDataArrived(AlignedFrameData obj)
     {
-        colorData = data.ColorData;
-        depthData = data.DepthData;
+        data = obj;
 
-        depth = depthData[y * 640 + x] / 1000f;
-        depthMedian = MedianDepth();
+        fpsCount++;
 
-        countColor++;
-
-        if ((DateTime.Now - dateColor).Seconds >= 1)
+        if ((DateTime.Now - fpsDate).Seconds >= 1)
         {
-            Debug.Log("Color: " + countColor);
+            Debug.Log("FPS: " + fpsCount);
 
-            dateColor = DateTime.Now;
-            countColor = 0;
+            fpsDate = DateTime.Now;
+            fpsCount = 0;
         }
-    }
-
-    private void Device_OnRecordingCompleted()
-    {
-        Debug.Log("Recording completed!");
-    }
-
-    private float MedianDepth()
-    {
-        const int minimum = 0;
-        const int maximum = 5000;
-
-        float result = depth;
-
-        List<ushort> values = new List<ushort>();
-
-        int xMin = x - radius >= 0 ? x - radius : 0;
-        int xMax = x + radius <= 640 ? x + radius : 640;
-        int yMin = y - radius >= 0 ? y - radius : 0;
-        int yMax = y + radius <= 480 ? y + radius : 480;
-
-        for (int iX = xMin; iX < xMax; iX++)
-        {
-            for (int iY = yMin; iY < yMax; iY++)
-            {
-                ushort current = depthData[iY * 640 + iX];
-
-                if (current > minimum && current < maximum)
-                {
-                    values.Add(current);
-                }
-            }
-        }
-
-        if (values.Count > 0)
-        {
-            foreach (ushort value in values)
-            {
-                result += value;
-            }
-
-            result /= values.Count;
-            result /= 1000f;
-        }
-
-        return result;
     }
 
     public void OnStartRecording()
     {
-        device.IsRecording = true;
+        //device.IsRecording = true;
     }
 
     public void OnStopRecording()
     {
-        device.IsRecording = false;
+        //device.IsRecording = false;
     }
 }
--- LightBuzz.RealSense/LightBuzz.RealSense/RealSenseDevice.cs
+++ LightBuzz.RealSense/LightBuzz.RealSense/RealSenseDevice.cs
@@ -15,10 +15,6 @@ namespace LightBuzz.RealSense
 
         private readonly AutoResetEvent _streamingEvent = new AutoResetEvent(false);
 
-        private readonly AutoResetEvent _recordingEvent = new AutoResetEvent(false);
-
-        private readonly AutoResetEvent _savingEvent = new AutoResetEvent(false);
-
         #endregion
 
         #region Members
@@ -26,12 +22,7 @@ namespace LightBuzz.RealSense
         private Pipeline _pipeline;
         private Align _aligner;
         private Thread _streamingThread;
-        private Thread _recordingThread;
-        private Thread _savingThread;
-
         private AlignedFrameData _frameData;
-
-        private bool _isRecording;
 
         #endregion
 
@@ -42,24 +33,14 @@ namespace LightBuzz.RealSense
         /// </summary>
         public event Action<AlignedFrameData> OnFrameDataArrived;
 
-        /// <summary>
-        /// Raised when all of the frames have been recorded and saved to disk.
-        /// </summary>
-        public event Action OnRecordingCompleted;
-
         #endregion
 
         #region Properties
 
         /// <summary>
-        /// Determines whether the device is streaming frames.
+        /// Determines whether the device is open and streaming frames.
         /// </summary>
-        public bool Streaming { get; protected set; }
-
-        /// <summary>
-        /// Determines whether a new frame set has arrived.
-        /// </summary>
-        public bool FrameDataArrived { get; protected set; }
+        public bool IsOpen { get; protected set; }
 
         /// <summary>
         /// The active profile of the RealSense camera.
@@ -75,40 +56,6 @@ namespace LightBuzz.RealSense
         /// The coordinate mapper of the current device.
         /// </summary>
         public CoordinateMapper CoordinateMapper { get; protected set; }
-
-        /// <summary>
-        /// Specifies whether the device will record color and depth data in the background.
-        /// </summary>
-        public bool IsRecording
-        {
-            get
-            {
-                return _isRecording;
-            }
-            set
-            {
-                _isRecording = value;
-
-                if (_isRecording)
-                {
-                    _recordingIndex = 0;
-
-                    //_recordingEvent.Reset();
-                    //_recordingThread = new Thread(RecordFrames)
-                    //{
-                    //    IsBackground = true
-                    //};
-                    //_recordingThread.Start();
-
-                    _savingEvent.Reset();
-                    _savingThread = new Thread(SaveFrames)
-                    {
-                        IsBackground = true
-                    };
-                    _savingThread.Start();
-                }
-            }
-        }
 
         #endregion
 
@@ -126,20 +73,16 @@ namespace LightBuzz.RealSense
                 DeviceConfiguration = DeviceConfiguration.Default();
             }
 
-            using (var cfg = DeviceConfiguration.ToPipelineConfig())
+            using (Config cfg = DeviceConfiguration.ToPipelineConfig())
             {
                 ActiveProfile = _pipeline.Start(cfg);
-            }
-
-            using (var activeStreams = ActiveProfile.Streams)
-            {
-                DeviceConfiguration.Profiles = activeStreams.Select(VideoStreamRequest.FromProfile).ToArray();
             }
 
             _aligner = new Align(Stream.Depth);
 
             _frameData = new AlignedFrameData
             {
+                Timestamp = DateTime.Now,
                 ColorData = new byte[DeviceConfiguration.ColorProfile.Width * DeviceConfiguration.ColorProfile.Height * 3],
                 DepthData = new ushort[DeviceConfiguration.DepthProfile.Width * DeviceConfiguration.DepthProfile.Height]
             };
@@ -153,7 +96,7 @@ namespace LightBuzz.RealSense
             };
             _streamingThread.Start();
 
-            Streaming = true;
+            IsOpen = true;
         }
 
         /// <summary>
@@ -163,38 +106,15 @@ namespace LightBuzz.RealSense
         {
             OnFrameDataArrived = null;
 
-            if (_recordingThread != null)
-            {
-                _recordingThread.Abort();
-                _recordingThread = null;
-            }
             if (_streamingThread != null)
             {
                 _streamingEvent.Set();
                 _streamingThread.Join();
             }
 
-            //if (_savingThread != null)
-            //{
-            //    _savingEvent.Set();
-            //    _savingThread.Join();
-            //}
-
-            //if (_recordingThread != null)
-            //{
-            //    _recordingEvent.Set();
-            //    _recordingThread.Join();
-            //}
-
-            //if (_streamingThread != null)
-            //{
-            //    _streamingEvent.Set();
-            //    _streamingThread.Join();
-            //}
-
             if (_pipeline != null)
             {
-                if (Streaming)
+                if (IsOpen)
                 {
                     _pipeline.Stop();
                 }
@@ -202,8 +122,6 @@ namespace LightBuzz.RealSense
                 _pipeline.Release();
                 _pipeline = null;
             }
-
-            Streaming = false;
 
             if (ActiveProfile != null)
             {
@@ -216,11 +134,8 @@ namespace LightBuzz.RealSense
                 _pipeline.Release();
                 _pipeline = null;
             }
-        }
 
-        public void Dispose()
-        {
-            _queue.Clear();
+            IsOpen = false;
         }
 
         #endregion
@@ -245,100 +160,7 @@ namespace LightBuzz.RealSense
                         depthFrame.CopyTo(_frameData.DepthData);
                     }
 
-                    if (_isRecording)
-                    {
-                        _queue.Enqueue(_frameData);
-                    }
-
                     OnFrameDataArrived?.Invoke(_frameData);
-
-                    FrameDataArrived = true;
-                }
-            }
-        }
-
-        private Queue<AlignedFrameData> _queue = new Queue<AlignedFrameData>();
-        private int _recordingIndex;
-        private DateTime _lastRecordedDate;
-
-        private void RecordFrames()
-        {
-            if (!_recordingEvent.WaitOne(0))
-            {
-                if (_isRecording)
-                {
-                    if (_frameData != null/* && _frameData.Timestamp != _lastRecordedDate*/)
-                    {
-                        //byte[] colorCopy = new byte[_frameData.ColorData.Length];
-                        //ushort[] depthCopy = new ushort[_frameData.DepthData.Length];
-
-                        //Array.Copy(_frameData.ColorData, colorCopy, colorCopy.Length);
-                        //Array.Copy(_frameData.DepthData, depthCopy, depthCopy.Length);
-
-                        //_queue.Enqueue(new AlignedFrameData
-                        //{
-                        //    Timestamp = _frameData.Timestamp,
-                        //    ColorData = colorCopy,
-                        //    DepthData = depthCopy
-                        //});
-
-                        AlignedFrameData d = new AlignedFrameData
-                        {
-                            Timestamp = _frameData.Timestamp,
-                            ColorData = _frameData.ColorData,
-                            DepthData = _frameData.DepthData
-                        };
-
-                        _queue.Enqueue(d);
-                        //_lastRecordedDate = _frameData.Timestamp;
-                    }
-                }
-                else
-                {
-                    if (_recordingThread != null)
-                    {
-                        _recordingEvent.Set();
-                        _recordingThread.Join();
-                    }
-                }
-            }
-        }
-
-        private void SaveFrames()
-        {
-            while (!_savingEvent.WaitOne(0))
-            {
-                if (_queue.Count > 0)
-                {
-                    AlignedFrameData data = _queue.Dequeue();
-
-                    string pathColor = System.IO.Path.Combine(@"C:\Users\Galini\Desktop\Video", _recordingIndex + "_color.bin");
-                    string pathDepth = System.IO.Path.Combine(@"C:\Users\Galini\Desktop\Video", _recordingIndex + "_depth.bin");
-
-                    System.IO.File.WriteAllBytes(pathColor, data.ColorData);
-
-                    using (System.IO.FileStream fs = new System.IO.FileStream(pathDepth, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write))
-                    {
-                        using (System.IO.BinaryWriter bw = new System.IO.BinaryWriter(fs))
-                        {
-                            foreach (ushort value in data.DepthData)
-                            {
-                                bw.Write(value);
-                            }
-                        }
-                    }
-
-                    _recordingIndex++;
-                }
-                else
-                {
-                    if (_savingThread != null)
-                    {
-                        OnRecordingCompleted?.Invoke();
-
-                        _savingEvent.Set();
-                        _savingThread.Join();
-                    }
                 }
             }
         }
